fb18676c9e0f737185d4c8af8003ab2440371157
Manmay <manmay3355@gmail.com>
2024-11-17 12:21:55 +0530

    Add some fornt fiture

--- EmployeeList/clint/src/component/Navbar.jsx
+++ EmployeeList/clint/src/component/Navbar.jsx
@@ -1,27 +1,52 @@
-import React from 'react'
-import './Navbar.css'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react';
+import './Navbar.css';
+import { NavLink } from 'react-router-dom';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <nav>
         <div className="container-Navbar">
-          <div className='logo'>
-            <a href='/' className='download-logo' alt="not working" ></a>
+          <div className="logo">
+            <a href="/" className="download-logo" alt="Logo"></a>
           </div>
-          <div className="navbar-link">
+          <div className="hamburger-menu" onClick={toggleMenu}>
+          <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/>
+</svg>
+          </div>
+          <div className={`navbar-link ${isOpen ? 'show' : ''}`}>
             <NavLink className="NavText" to="/">MEN</NavLink>
             <NavLink className="NavText" to="/about">WOMEN</NavLink>
             <NavLink className="NavText" to="/about">KIDS</NavLink>
-            <input type="text" className='Search-Bar' placeholder='Search for product' />
-            <a href="" className='search'><i class="fa-solid fa-magnifying-glass"></i></a>
+            <form className="search-form max-w-md mx-auto">
+              <label htmlFor="default-search" className="sr-only">Search</label>
+              <div className="relative w-full">
+                <input
+                  type="search"
+                  id="default-search"
+                  className="Search-Bar p-4 text-sm border border-gray-300 rounded-lg"
+                  placeholder="Search Product..."
+                  required
+                />
+                <button type="submit">
+                  Search
+                </button>
+              </div>
+            </form>
+
             <NavLink className="NavText-login" to="/login">LOGIN</NavLink>
           </div>
         </div>
       </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- EmployeeList/clint/src/pages/Login.jsx
+++ EmployeeList/clint/src/pages/Login.jsx
@@ -1,27 +1,25 @@
-import { useState } from 'react'
-import React from 'react'
-import './Login.css'
-
+import React, { useState } from 'react';
+import './Login.css';
 
 function Login() {
   const [ColorChanged, setColorChange] = useState(false);
+
   const handleClick = () => {
     setColorChange(!ColorChanged);
   };
+
   return (
-    <>
-    <div className={`login ${ColorChanged ? 'bg-changed' : ''}`}  onClick={handleClick}>
+    <div className={`login ${ColorChanged ? 'bg-changed' : ''}`} onClick={handleClick}>
       <div className="card">
-        <label className='LoginHeader'>Login or Signup</label>
-        <label htmlFor="">Name</label>
-        <input type="text" placeholder='Enter your name' />
-        <label htmlFor="">Password</label>
-        <input type="password" placeholder='Enter your password' />
-        <button className='LoginButton'>Login</button>
+        <label className="LoginHeader">Login or Signup</label>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" placeholder="Enter your name" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" placeholder="Enter your password" />
+        <button className="LoginButton">Login</button>
       </div>
     </div>
-    </>
-  )
+  );
 }
 
-export default Login
+export default Login;
